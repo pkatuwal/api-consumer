826a74f3cad141279b2b92a770178dcf688c8319
Pramod Katuwal <pramod.katuwal@worldlink.com.np>
2020-07-04 19:14:05 +0545

    add extra test key with default config

--- config/api-consumer.php
+++ config/api-consumer.php
@@ -7,10 +7,11 @@ return [
     'default'=>[
         'timeout'=>10,
         'ssl_verification'=>true,
+        'method'=>"GET"
     ],
     'consumer' => [
         'ebill' => [
-            'baseuri' => 'https://api-ebill.wlink.com.np/',
+            'baseUri' => 'https://api-ebill.wlink.com.np/',
             'version' => 'v1',
             'secret' => 'bearer eyj0exaioijkv1qilcjhbgcioijiuzi1nij9.eyjpc3mioijodhrwczovl2fwas1lymlsbc53bgluay5jb20ubnavl3yxl2xvz2luiiwiawf0ijoxntq1mdi4odu5lcjuymyioje1nduwmjg4ntksimp0asi6ikxkbezuekppughlwgxjzluilcjzdwiioijlc2vydmljzs5hzg1pbiisinbydii6ijizymq1yzg5ndlmnjawywrimzllnzaxyzqwmdg3mmrin2e1otc2zjcilcjhchbsawnhdglvbii6imvzzxj2awnlin0.rr7nnryjrl9yyok6c5dzvhbow1nvgl0zzjlm4lw_grs',
             'timeout'=>10,
@@ -25,5 +26,9 @@ return [
             // ],
             // 'notify' => true
         ],
+        'covid19api'=>[
+            'baseUri'=>'https://api.covid19api.com/',
+            'timeout'=>10
+        ]
     ]
 ];
