9021f8e9ef6b91a49b6d6dc9e1ee0847cce58d98
Pramod Katuwal <pramod.katuwal@worldlink.com.np>
2020-07-04 20:38:30 +0545

    fix custom consumers packge

--- config/api-consumer.php
+++ config/api-consumer.php
@@ -16,7 +16,7 @@ return [
             'secret' => 'bearer eyj0exaioijkv1qilcjhbgcioijiuzi1nij9.eyjpc3mioijodhrwczovl2fwas1lymlsbc53bgluay5jb20ubnavl3yxl2xvz2luiiwiawf0ijoxntq1mdi4odu5lcjuymyioje1nduwmjg4ntksimp0asi6ikxkbezuekppughlwgxjzluilcjzdwiioijlc2vydmljzs5hzg1pbiisinbydii6ijizymq1yzg5ndlmnjawywrimzllnzaxyzqwmdg3mmrin2e1otc2zjcilcjhchbsawnhdglvbii6imvzzxj2awnlin0.rr7nnryjrl9yyok6c5dzvhbow1nvgl0zzjlm4lw_grs',
             'timeout'=>10,
             // 'custom_consumer' => [
-            //     'app\\consumers\\ebill'
+            //     'Consumers\\covid'
             // ],
             // 'on' => [
             //     'exception' => [
@@ -28,6 +28,9 @@ return [
         ],
         'covid19api'=>[
             'baseUri'=>'https://api.covid19api.com/',
+            'custom_consumer' => [
+                'Consumers/covid'
+            ],
             'timeout'=>10
         ]
     ]
