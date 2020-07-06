1751463093a5bfb2386449a5e9027e711df6b363
Pramod Katuwal <pramod.katuwal@worldlink.com.np>
2020-07-06 22:18:09 +0545

    refactor existing request code, handle json payload too

--- src/Traits/ConsumerHttpClient.php
+++ src/Traits/ConsumerHttpClient.php
@@ -13,17 +13,26 @@ trait ConsumerHttpClient
             $client = new Client([
                 'base_uri' => $this->baseUri
             ]);
-
-            $response = $client->request($this->method, $this->requestUri, [
-                'form_params' => $this->formParams,
-                'headers' => $this->headers,
-                'timeout'=>$this->timeout,
-                'verify'=>$this->sslVerify
-            ]);
+            $response = $client->request($this->method, $this->requestUri, $this->getGuzzleOptions());
             return json_decode($response->getBody()->getContents(), true);
         } catch (\GuzzleHttp\Exception\RequestException $e) {
             $errorCode = 500;
             throw new \Exception($e->getResponse()->getBody()->getContents(), $errorCode);
         }
+    }
+
+    private function getGuzzleOptions()
+    {
+        $options=[
+            'headers' => $this->headers,
+            'timeout'=>$this->timeout,
+            'verify'=>$this->sslVerify
+        ];
+        if (isset($this->graphQl) && $this->graphQl!==null) {
+            $options[\GuzzleHttp\RequestOptions::JSON]=$this->graphQl;
+            return $options;
+        }
+        $options['form_params']=$this->formParams;
+        return $options;
     }
 }
